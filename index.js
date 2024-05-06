34a44ccd68c2c7e8ec815da3ef9a378b4ec208de
Nehal Yatham <nehal@Nehals-MacBook-Pro.local>
2024-05-05 21:09:22 -0400

    Till Exercise 3.6 finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
   { 
@@ -24,12 +25,67 @@ let persons = [
   }
 ]
 
-app.get('/', (req, res) => res.send('<h1> Hello World! </h1>'))
+const generateId = () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/', (req, res) => {
+  res.send('<h1> Hello World! </h1>')
 })
 
+app.get('/api/persons', (req, res) => res.json(persons))
+
+app.get('/info', (req, res) => res.send(`
+  <p> Phonebook has info for ${persons.length} people </p>
+  <p> ${new Date()} </p>
+  `)
+)
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const isIdValid = persons.some(person => person.id === id)
+  if (! isIdValid) {
+    return res.status(404).json({
+      Error: `ID: ${id} not found`
+    })
+  }
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  // Error Handling
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      Error: 'Name or Number missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return res.status(400).json({
+      Error: 'Name must be unique'
+    })
+  }
+
+  // If the cases pass, we are gonna make a new entry
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+  persons = [...persons, person]
+  res.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => console.log('Hello World, App is currently running on PORT', PORT))
